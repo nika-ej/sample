d13f189d8efe22a57a0244ffb6cce0c7ab8b6387
nika-ej <dmswlluv@gmail.com>
2025-06-27 00:33:31 +0900

    dev 브랜치에서 출력수정

--- sample/src/sample/Sample.java
+++ sample/src/sample/Sample.java
@@ -3,8 +3,7 @@ package sample;
 public class Sample {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello github!!!!");
-		System.out.println("Hello from branch Dev");
+		System.out.println("Hello from branch Dev github!!!!");
 	}
 	
 }
