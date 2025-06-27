616fb6ec1b3a6b520737be3c9727c1fc8daa9f29
nika-ej <dmswlluv@gmail.com>
2025-06-28 00:31:35 +0900

    test1111

--- sample/src/sample/Sample.java
+++ sample/src/sample/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("A 개발자 작업입니다.");
 		System.out.println("B 개발자 작업입니다.");
+		System.out.println("test11111111111111");
 	}
 
 }
